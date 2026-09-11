aeccf93d3d99fe9b039a5786a9b6e79e7f502681
IU25CA094-THRISHA <thrishathrisha01033@gmail.com>
2026-09-11 09:43:22 +0530

    Update solution.sql

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -1,23 +1,4 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
+insert into Student values(1001,"Arun","Male",101),(1002,"Divya","Female",102),(1003,"Karthik","Male",101);
+select * from Student;
 
-CREATE TABLE Student(
-    StudentID INT(5) PRIMARY KEY,
-    StudentName VARCHAR(20) NOT NULL,
-    DOB DATE,
-    Gender VARCHAR(10),
-    DepartmentID INT(5),
-    Email VARCHAR(30),
-    PhoneNumber BIGINT
-);
 
--- Insert
-
--- 1001 Arun
-
--- 1002 Divya
-
--- 1003 Karthik
-
--- Display all records
